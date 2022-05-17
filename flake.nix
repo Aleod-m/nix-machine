6882123f3d2b2969bba3246d2328072f79231bab
AdrienDML <adriendml99@protonmail.com>
2022-05-17 08:01:19 +0200

    current

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         inherit system pkgs;
         username = "adml";
         homeDirectory = "/home/adml"; 
-        configuration = ./users/adml/home.nix;
+        configuration = {imports = [./users/adml/home.nix];};
       };
     };
 
