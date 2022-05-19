1e86364972761bf84876f5477cc8214f1bd3d257
AdrienDML <adriendml99@protonmail.com>
2022-05-19 10:45:32 +0200

    Moved software from system conf

--- users/adml/home.nix
+++ users/adml/home.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
+    discord
+    spotify
+
     bat
     exa
     zoxide
@@ -9,14 +12,19 @@
 
     unzip
     wget
-    vlc
     htop
+
+    vlc
+    obs-studio
     
     gimp
     inkscape
+
+    multimc
   ];
   home.username = "adml";
   home.homeDirectory = "/home/adml";
+
   programs = {
     home-manager.enable = true;
     bash = {
@@ -24,10 +32,13 @@
       shellAliases = import ./shellAliases.nix {inherit pkgs;};
       
     };
+
     git = {
       enable = true;
       userName = "AdrienDML";
       userEmail = "adriendml99@gmail.com";
     };
   };
+
+  services.xserver.layout = ;
 }
