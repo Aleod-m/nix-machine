fb585b41aa6d45d0241cddea4198383cadc7c81c
AdrienDML <adriendml99@protonmail.com>
2022-05-20 05:34:18 +0200

    Nvidia try 2

--- system/configuration.nix
+++ system/configuration.nix
@@ -45,7 +45,9 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-  #hardware.opengl.enable = true;
+  hardware.opengl.enable = true;
+  hardware.nvidia.modesetting.enable = true;
+  
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm = {
