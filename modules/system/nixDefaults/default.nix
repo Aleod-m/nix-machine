bda65ddb2646ce02939697575023f1952da58707
AdrienDML <adriendml99@protonmail.com>
2022-09-17 07:42:31 +0200

    Fixed modules/system/nixDefaults module and added flexibility.

--- modules/system/nixDefaults/default.nix
+++ modules/system/nixDefaults/default.nix
@@ -1,24 +1,47 @@
 _: { pkgs, lib, config, ... }: 
 let
   cfg = config.nix;
+  types = lib.types;
 in {
   options = {
-    nix.allowedUnfree = lib.mkOption {
-      type = lib.types.listOf lib.types.string;
-      default = [];
-      description = ''
-        Allows for  unfree packages by their name.
-      '';
+    nix = {
+      allowedUnfree = lib.mkOption {
+        type = types.listOf types.string;
+        default = [];
+        description = ''
+          Allows for  unfree packages by their name.
+        '';
+      };
+      flakes.enable = lib.mkOption {
+        type = types.boolean;
+        default = true;
+        description = ''
+          Enable nix flakes.
+        '';
+      };
+      gc.enable = lib.mkEnableOption {
+        type = types.boolean;
+        default = true;
+        description = ''
+          Enable nix flakes.
+        '';
+      };
     };
   };
-  config = {
-    nix.package                = pkgs.nixFlakes;
-    nix.extraOptions           = '' experimental-features = nix-command flakes '';
-    nix.autoOptimiseStore      = true;
-    nixpkgs.config.allowUnfree = true;
-    nix.gc.automatic = lib.mkDefault true;
-    nix.settings.auto-optimise-store = lib.mkDefault true;
-    nix.gc.options = lib.mkDefault "--delete-older-than 10d";
-    nixpkgs.config.allowUnfreePredicate = lib.mkIf (cfg.allowedUnfree != []) (pkg: __elem (lib.getName pkg) allowed);
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.flakes.enable {
+      nix.package                = pkgs.nixFlakes;
+      nix.extraOptions           = '' experimental-features = nix-command flakes '';
+    })
+    (lib.mkIf cfg.gc.enable {
+      nix.autoOptimiseStore = true;
+      nix.settings.auto-optimise-store = lib.mkDefault true;
+      nix.gc.automatic = lib.mkDefault true;
+      nix.gc.options = lib.mkDefault "--delete-older-than 10d";
+    })
+    (lib.mkIf (cfg.allowedUnfree != []) {
+      nixpkgs.config.allowUnfree = true;
+      nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) config.allowedUnfree;
+    })
+  ];
 }
