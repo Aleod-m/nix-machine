b02eb38a08bcfc3de1d8aab5a51e26d4b73c20bd
AdrienDML <adriendml99@protonmail.com>
2023-11-28 09:57:48 +0100

    installed just ts

--- users/adml/config/nvim/lua/aleod/plugins/treesitter.lua
+++ users/adml/config/nvim/lua/aleod/plugins/treesitter.lua
@@ -1,69 +1,70 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    event = { "BufReadPre", "BufNewFile" },
-    build = ':TSUpdate',
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            highlight = {
-                enable = true,
-                disable = {""},
-            },
+  'nvim-treesitter/nvim-treesitter',
+  event = { "BufReadPre", "BufNewFile" },
+  build = ':TSUpdate',
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      highlight = {
+        enable = true,
+        disable = {""},
+      },
 
-            indent = { enable = true, },
-            ensure_installed = {
-                -- Main languages.
-                "rust",
-                "nix",
-                "lua",
-                --"Godot",
+      indent = { enable = true, },
+      ensure_installed = {
+        -- Main languages.
+        "rust",
+        "nix",
+        "lua",
+        --"Godot",
 
-                -- Secondary languages.
-                "c",
-                "cpp",
-                "bash",
-                "zig",
-                "python",
-                "sql",
-                "wgsl",
-                "wgsl_bevy",
+        -- Secondary languages.
+        "c",
+        "cpp",
+        "bash",
+        "zig",
+        "python",
+        "sql",
+        "wgsl",
+        "wgsl_bevy",
 
-                -- Web.
-                "html",
-                "css",
-                "scss",
-                "javascript",
+        -- Web.
+        "html",
+        "css",
+        "scss",
+        "javascript",
 
-               -- Data formats.
-                "toml",
-                "xml",
-                "json",
-                "yaml",
-            },
-        })
+        -- Data formats.
+        "toml",
+        "xml",
+        "json",
+        "yaml",
+      },
+    })
 
-        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-        parser_config.nu = {
-              install_info = {
-                url = "https://github.com/nushell/tree-sitter-nu",
-                files = { "src/parser.c" },
-                branch = "main",
-            },
-            filetype = "nu",
-        }
-        parser_config.wgsl = {
-            install_info = {
-                url = "szebniok/tree-sitter-wgsl",
-                files = {"src/parser.c"}
-            },
-            filetype = "justfile"
-        }
-        parser_config.just = {
-            install_info = {
-                files = { "src/parser.c", "src/scanner.c" },
-                branch = "main",
-            },
-            filetype = "justfile"
-        }
-    end
+    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+    parser_config.nu = {
+      install_info = {
+        url = "https://github.com/nushell/tree-sitter-nu",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "nu",
+    }
+    parser_config.wgsl = {
+      install_info = {
+        url = "szebniok/tree-sitter-wgsl",
+        files = {"src/parser.c"}
+      },
+      filetype = "wgsl"
+    }
+    parser_config.just = {
+      install_info = {
+        url = "https://github.com/IndianBoy42/tree-sitter-just",
+        files = { "src/parser.c", "src/scanner.c" },
+        branch = "main",
+      },
+      filetype = "justfile"
+    }
+  end
 }
