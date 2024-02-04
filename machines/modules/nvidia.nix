b23ce3b58f45be7ad6f83ee1c2eba64acc78bd27
AdrienDML <adriendml99@protonmail.com>
2024-02-04 16:20:12 +0100

    Clean up nvidia module

--- machines/modules/nvidia.nix
+++ machines/modules/nvidia.nix
@@ -2,32 +2,22 @@
   config,
   pkgs,
   ...
-}: let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec -a "$0" "$@"
-  '';
-in {
+}: {
   nix.allowedUnfree = ["nvidia-x11"];
   hardware = {
     opengl = {
-      enable =
-        true;
-      driSupport32Bit =
-        true;
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
     };
     nvidia = {
+      modesetting.enable = true;
+      open = false;
       prime = {
-        sync.enable =
-          true;
+        sync.enable = true;
       };
     };
   };
   services.xserver.videoDrivers = ["nvidia"];
-  environment.sessionVariables =
-    {__GLX_VENDOR_LIBRARY_NAME = "nvidia";}
-    #; boot.kernelParams = [ "module_blacklist=i915" ]
-    ;
+  environment.sessionVariables = {__GLX_VENDOR_LIBRARY_NAME = "nvidia";};
 }
