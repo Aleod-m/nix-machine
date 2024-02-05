0e06b864a64d161f973d0a2ae55213d8e34aeaa6
AdrienDML <adriendml99@protonmail.com>
2024-02-05 08:27:27 +0100

    Changed the shell and formater to be platform agnostic.

--- flake.nix
+++ flake.nix
@@ -12,33 +12,31 @@
   outputs = {
     self,
     nixpkgs,
+    flake-utils,
     ...
   } @ inputs: let
     devPkgs = (import nixpkgs) {system = "x86_64-linux";};
-  in {
-    # All the programs i need to edit my config.
-    devShells.x86_64-linux.default = devPkgs.mkShell {
-      packages = with devPkgs; [
-        # The nix lsp i use.
-        rnix-lsp
-        # The lua lsp for my neovim config
-        lua-language-server
-        # to run the nix flake commands.
-        just
-      ];
-      DIRENV_LOG_FORMAT = "";
-    };
+    generic = flake-utils.lib.eachDefaultSystem (system: {
+      devShells.default = devPkgs.mkShell {
+        # All the programs i need to edit my config.
+        packages = with devPkgs; [
+          # The nix lsp i use.
+          rnix-lsp
+          # The lua lsp for my neovim config
+          lua-language-server
+          # to run the nix flake commands.
+          just
+        ];
+        DIRENV_LOG_FORMAT = "";
+      };
 
+      formatter = devPkgs.alejandra;
+    });
+  in generic // {
+    inherit (import ./modules) nixosModules homeManagerModules mixedModules;
+
+    # Extend nixpkgs library.
     lib = import ./lib nixpkgs;
-
-    inherit
-      (import ./modules)
-      nixosModules
-      homeManagerModules
-      mixedModules
-      ;
-
-    formatter.x86_64-linux = devPkgs.alejandra;
 
     homeConfigurations = import ./users inputs;
 
