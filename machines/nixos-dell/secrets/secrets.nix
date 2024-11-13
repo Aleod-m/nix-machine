84ce8b66e19d28d2a52379ad943f43561bbc85ab
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2024-11-13 09:49:48 +0100

    fix secrets

--- machines/nixos-dell/secrets/secrets.nix
+++ machines/nixos-dell/secrets/secrets.nix
@@ -1,9 +1,10 @@
 let
   workKey = __readFile ./work.key.pub;
+  nixos-dellKey = __readFile ./work.key.pub;
   # persoKey = __readFile ./perso.key.pub;
 in {
-  "bibvpn_ca.crt.age".publicKeys = [workKey];
-  "bibvpn_crt.crt.age".publicKeys = [workKey];
-  "bibvpn_key.key.age".publicKeys = [workKey];
-  "bibvpn_ta.key.age".publicKeys = [workKey];
+  "bibvpn_ca.crt.age".publicKeys = [workKey nixos-dellKey];
+  "bibvpn_crt.crt.age".publicKeys = [workKey nixos-dellKey];
+  "bibvpn_key.key.age".publicKeys = [workKey nixos-dellKey];
+  "bibvpn_ta.key.age".publicKeys = [workKey nixos-dellKey];
 }
