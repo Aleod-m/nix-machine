96a029baf907b36cda75d3f6a67a4a2fe52bf325
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2024-12-02 15:48:36 +0100

    Add grep arguments telescope extention

--- users/modules/nvim/config/lua/aleod/plugins/telescope.lua
+++ users/modules/nvim/config/lua/aleod/plugins/telescope.lua
@@ -1,15 +1,17 @@
 local leader = require('core.keymaps').leader
+local cmd = require('core.cmd')
 return {
   'nvim-telescope/telescope.nvim',
   lazy = false,
   dependencies = { 
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-live-grep-args.nvim" ,
   },
   keys = {
     { mode= 'n', leader 'ff',  require('telescope.builtin').find_files  },
     { mode= 'n', leader 'fs',  require('telescope.builtin').lsp_dynamic_workspace_symbols},
-    { mode= 'n', leader 'fgs',  require('telescope.builtin').live_grep  },
+    { mode= 'n', leader 'fgs',  cmd "lua require('telescope').extensions.live_grep_args.live_grep_args()"},
     { mode= 'n', leader 'fgf', require('telescope.builtin').git_files   },
     { mode= 'n', leader 'fgb', require('telescope.builtin').git_branches},
     { mode= 'n', leader 'fb',  require('telescope.builtin').buffers     },
@@ -22,6 +24,7 @@ return {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown { }
         },
+        live_grep_args = {}
       },
       pickers = {
         current_buffer_fuzzy_find = {
@@ -34,5 +37,6 @@ return {
       },
     })
     tele.load_extension('ui-select')
+    tele.load_extension('telescope-live-grep-args')
   end,
 }
