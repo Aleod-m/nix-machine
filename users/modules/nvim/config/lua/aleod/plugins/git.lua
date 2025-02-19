525863341aca23afda20d847225ebc3cb6a8fd22
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2025-02-19 14:58:36 +0100

    Nvim: Shorten line blame appearance delay

--- users/modules/nvim/config/lua/aleod/plugins/git.lua
+++ users/modules/nvim/config/lua/aleod/plugins/git.lua
@@ -20,6 +20,7 @@ return {
     opts = {
       numhl = true,
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = { delay = 500 },
     }
   },
 }
