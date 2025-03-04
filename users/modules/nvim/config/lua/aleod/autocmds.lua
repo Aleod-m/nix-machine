d3f6cc7ae4a702d1bd6e81e05404f105512d8e25
AdrienDML <adriendml99@protonmail.com>
2025-03-04 19:36:33 +0100

    Added autocmd becaus ftplugin is inconsistent

--- users/modules/nvim/config/lua/aleod/autocmds.lua
+++ users/modules/nvim/config/lua/aleod/autocmds.lua
@@ -1,5 +1,13 @@
 local group = vim.api.nvim_create_augroup('AleodConfig', {clear = true})
 
+vim.api.nvim_create_autocmd({"BufEnter"}, {
+  group = group,
+  filetype = "help",
+  callback = function()
+    if vim.bo.filetype ~= "help" then return end
+    vim.cmd.wincmd('L')
+  end
+})
 -- Add autocmd to try reloading all buffers on CursorHold.
 vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
   group = group,
