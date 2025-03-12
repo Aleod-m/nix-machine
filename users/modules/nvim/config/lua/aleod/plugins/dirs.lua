6e1513772f2aa878d0037ca16acb7208961fa72b
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2025-03-12 09:55:44 +0100

    Removed old commented plugin.

--- users/modules/nvim/config/lua/aleod/plugins/dirs.lua
+++ users/modules/nvim/config/lua/aleod/plugins/dirs.lua
@@ -1,6 +1,4 @@
 local cmd = require'core.cmd'
-local group = require('aleod')
-
 return {
   'stevearc/oil.nvim',
   lazy = false,
@@ -12,27 +10,3 @@ return {
   },
   config = true,
 }
---    {
---  'natecraddock/workspaces.nvim',
---  lazy = false,
---  dependencies = {
---    'nvim-telescope/telescope.nvim',
---    'stevearc/oil.nvim',
---  },
---  config = function(_, _)
---    require('workspaces').setup({
---      hooks = {
---        open = 'Oil .',
---      }
---    })
---    require('telescope').load_extension('workspaces')
---    vim.api.nvim_create_autocmd({"UIEnter"}, {
---      group = group,
---      callback = function(args) 
---        if #(vim.v.argv) == 0 then
---          vim.cmd[[Telescope workspaces]]
---        end
---      end
---    })
---  end
---} 
