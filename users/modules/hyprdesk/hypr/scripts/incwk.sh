193a7de1deca3478cc4097745b4c8efee251ff72
AdrienDML <adriendml99@protonmail.com>
2025-04-07 16:43:32 +0200

    fix shell scripts

--- users/modules/hyprdesk/hypr/scripts/incwk.sh
+++ users/modules/hyprdesk/hypr/scripts/incwk.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 focused_monitor=$(hyprctl activewindow -j | jq '.monitor')
 curr_ws=$(hyprctl monitors -j | jq '.[0].activeWorkspace.id')
-next_ws=$(($curr_ws % 10 + 1))
+next_ws=$((curr_ws % 10 + 1))
+hyprctl dispatch -- focusmonitor 0
 hyprctl dispatch -- workspace $next_ws
-hyprctl dispatch -- workspace $(($next_ws + 10))
+hyprctl dispatch -- focusmonitor 1
+hyprctl dispatch -- workspace $((next_ws + 10))
+hyprctl dispatch -- focusmonitor "$focused_monitor"
