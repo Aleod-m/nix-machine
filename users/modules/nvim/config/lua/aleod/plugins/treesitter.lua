acdbeef6752b1dc4cf2c40651561bfad0cdf747b
AdrienDML <adriendml99@protonmail.com>
2025-04-29 04:59:10 +0200

    [Nvim] Remove just ts grammar

--- users/modules/nvim/config/lua/aleod/plugins/treesitter.lua
+++ users/modules/nvim/config/lua/aleod/plugins/treesitter.lua
@@ -59,17 +59,6 @@ return { {
         "yaml",
       },
     })
-
-    ---@class ParserInfo[]
-    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-    parser_config.just = {
-      install_info = {
-        url = "https://github.com/IndianBoy42/tree-sitter-just",
-        files = { "src/parser.c", "src/scanner.cc" },
-        branch = "main",
-      },
-      filetype = "justfile"
-    }
   end
 }, {
   "nushell/tree-sitter-nu",
