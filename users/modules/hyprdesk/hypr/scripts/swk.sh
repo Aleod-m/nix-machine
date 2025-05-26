adc8cd8f197a2e7114f2726138c8af2c1315c48a
AdrienDML <adriendml99@protonmail.com>
2025-05-26 09:19:24 +0200

    Fix switch workspace script

--- users/modules/hyprdesk/hypr/scripts/swk.sh
+++ users/modules/hyprdesk/hypr/scripts/swk.sh
@@ -2,12 +2,10 @@
 
 focused_monitor=$(hyprctl activewindow -j | jq '.monitor')
 while read -r id; do
-    hyprctl dispatch -- focuse_monitor "$id"
-    hyprctl dispatch -- workspace $((id * 10 + $1))
+    echo "focus $id"
+    hyprctl dispatch -- focusmonitor "$id"
+    echo "change $((id * 10 + $1))"
+    hyprctl dispatch -- workspace "$((id * 10 + $1))"
 done < <(hyprctl monitors -j | jq ".[] | .id")
+echo "restore focus"
 hyprctl dispatch -- focusmonitor "$focused_monitor"
-
-# hyprctl dispatch -- focusmonitor 0
-# hyprctl dispatch -- workspace "$1"
-# hyprctl dispatch -- focusmonitor 1
-# hyprctl dispatch -- workspace $((10 + $1))
