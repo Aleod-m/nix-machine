8e41db1a4eb0079e9e02c404a844d89f3a3669eb
AdrienDML <adriendml99@protonmail.com>
2025-06-26 10:22:05 +0200

    Added: fzf. TODO: fzf config.

--- users/modules/shell-utils.nix
+++ users/modules/shell-utils.nix
@@ -3,7 +3,18 @@
   config,
   mlib,
   ...
-}:
+}: let
+  bg = "191919";
+  green  = "97e023";
+  orange = "fa8419";
+  yellow = "dfd561";
+  purple = "9c64fe";
+  red    = "f3005f";
+  cyan   = "57d1ea";
+  blue   = "0e6172";
+  grey   = "929276";
+  white  = "f6f6ee";
+in
 mlib.enable.programs ["btop" "zoxide" "starship"] {
   home.packages = with pkgs; [
     bat
@@ -14,5 +25,23 @@ mlib.enable.programs ["btop" "zoxide" "starship"] {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+  };
+  programs.fzf = {
+    enable = true;
+    colors = {
+      inherit bg;
+      "bg+"="#293739";
+      border=white;
+      spinner="#E6DB74";
+      hl="#7E8E91";
+      fg="#F8F8F2";
+      header="#7E8E91";
+      info="#A6E22E";
+      pointer="#A6E22E";
+      marker="#F92672";
+      "fg+"="#F8F8F2";
+      prompt=white;
+      "hl+"="#F92672";
+    };
   };
 }
