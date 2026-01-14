f8acc25b76f73270cf90781f9413bcb5347f48c2
Aleod-m <adriendml99@protonmail.com>
2026-01-14 16:32:24 +0100

    [Nvim]: update ebnf grammar

--- users/modules/nvim/config/lua/aleod/plugins/treesitter.lua
+++ users/modules/nvim/config/lua/aleod/plugins/treesitter.lua
@@ -1,72 +1,87 @@
-return { {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    "tadmccorkle/markdown.nvim",
-  },
-  event = { "BufReadPre", "BufNewFile" },
-  build = ':TSUpdate',
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    ---@diagnostic disable-next-line: missing-fields
-    configs.setup({
-      highlight = {
-        enable = true,
-        disable = {""},
-      },
-      markdown = { enable = true, },
-
-      indent = { enable = true, },
-      ensure_installed = {
-        -- Noice
-        "vim",
-        "regex",
-        "markdown",
-        "markdown_inline",
-
-        "php",
-        "perl",
-
-        -- Programming languages.
-        "rust",
-        "nix",
-        "lua",
-        "luadoc",
-        "c",
-        "cpp",
-        "bash",
-        "zig",
-        "python",
-        "sql",
-        "wgsl",
-        "wgsl_bevy",
-        "java",
-        "javadoc",
-
-        -- Web.
-        "html",
-        "angular",
-        "css",
-        "scss",
-        "javascript",
-        "typescript",
-
-        -- Godot.
-        "gdscript",
-        -- "gdresource", Causes an error for some reason.
-        "gdshader",
-
-        -- Data formats.
-        "toml",
-        "xml",
-        "json",
-        "yaml",
-      },
-    })
-  end
-}, {
-  "nushell/tree-sitter-nu",
-  build = ":TSUpdate",
-  dependencies = {
+return {
+  {
     'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      "tadmccorkle/markdown.nvim",
+    },
+    event = { "BufReadPre", "BufNewFile" },
+    build = ':TSUpdate',
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+      ---@type table<string, any>
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.ebnf = {
+        install_info = {
+          url = 'https://github.com/Aleod-m/tree-sitter-ebnf', -- local path or git repo
+          location = 'parser',
+          generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+          requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+        },
+        filetype = "ebnf", -- if filetype does not match the parser name
+      }
+      vim.treesitter.language.register("ebnf", "ebnf")
+      ---@diagnostic disable-next-line: missing-fields
+      configs.setup({
+        highlight = {
+          enable = true,
+          disable = {""},
+        },
+        markdown = { enable = true, },
+
+        indent = { enable = true, },
+        ensure_installed = {
+          -- Noice
+          "vim",
+          "regex",
+          "markdown",
+          "markdown_inline",
+
+          "php",
+          "perl",
+
+          -- Programming languages.
+          "rust",
+          "nix",
+          "lua",
+          "luadoc",
+          "c",
+          "cpp",
+          "bash",
+          "zig",
+          "python",
+          "sql",
+          "wgsl",
+          "wgsl_bevy",
+          "java",
+          "javadoc",
+
+          -- Web.
+          "html",
+          "angular",
+          "css",
+          "scss",
+          "javascript",
+          "typescript",
+
+          -- Godot.
+          "gdscript",
+          -- "gdresource", Causes an error for some reason.
+          "gdshader",
+
+          -- Data formats.
+          "toml",
+          "xml",
+          "json",
+          "yaml",
+        },
+      })
+    end
   },
-} }
+  {
+    "aleod-m/tree-sitter-ebnf",
+    build = ":TSUpdate",
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+  }
+}
