426ea1a8a9768842e93a81476ea33cb3e0441e4a
Aleod-m <adriendml99@protonmail.com>
2026-01-21 10:01:01 +0100

    Formating

--- machines/nixos-dell/default.nix
+++ machines/nixos-dell/default.nix
@@ -7,27 +7,31 @@
   nixpkgs.config.allowUnfree = true;
   # Imports the hardware configuration
   imports = [./hardware.nix ./bibvpn.nix];
-  environment.systemPackages = with pkgs; [ecryptfs];
   fonts.packages = with pkgs.nerd-fonts; [zed-mono jetbrains-mono];
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
   };
-
   system.stateVersion = "24.05";
 
   # Bootloader.
-	boot.enableContainers = true; 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  services.fwupd.enable = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.initrd.systemd.enable = true;
-  services.printing.enable = true;
+  boot = {
+		kernelPackages = pkgs.linuxPackages_latest;
+		initrd.systemd.enable = true;
+		enableContainers = true; 
+		loader = {
+			systemd-boot.enable = true;
+			efi.canTouchEfiVariables = true;
+		};
+	};
 
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
+  services { 
+		fwupd.enable = true;
+  	printing.enable = true;
+  	avahi = {
+			enable = true;
+			nssmdns4 = true;
+			openFirewall = true;
+		};
+	};
 }
