55b70b1f7f7c47b3dd8d8920596d6374510221f9
Aleod-m <adriendml99@protonmail.com>
2026-03-10 09:12:44 +0100

    Reenable animations

--- users/modules/hyprdesk/hypr/default.nix
+++ users/modules/hyprdesk/hypr/default.nix
@@ -1,106 +1,122 @@
 {
-  pkgs,
-  config,
-  ...
+pkgs,
+config,
+...
 }: let
-  green = "rgb(97e023)";
-  orange = "rgb(fa8419)";
-  yellow = "rgb(dfd561)";
-  purple = "rgb(9c64fe)";
-  red = "rgb(f3005f)";
-  cyan = "rgb(57d1ea)";
-  blue = "rgb(0e6172)";
-  grey = "rgb(929276)";
-  white = "rgb(f6f6ee)";
+	green = "rgb(97e023)";
+	orange = "rgb(fa8419)";
+	yellow = "rgb(dfd561)";
+	purple = "rgb(9c64fe)";
+	red = "rgb(f3005f)";
+	cyan = "rgb(57d1ea)";
+	blue = "rgb(0e6172)";
+	grey = "rgb(929276)";
+	white = "rgb(f6f6ee)";
 
-  pointer = config.home.pointerCursor;
-  cursorName = "Bibata-Modern-Classic-Hyprcursor";
+	pointer = config.home.pointerCursor;
+	cursorName = "Bibata-Modern-Classic-Hyprcursor";
 in {
-  imports = [
-    ./binds.nix
-    ./rules.nix
-  ];
+	imports = [
+		./binds.nix
+		./rules.nix
+	];
 
-  xdg.configFile = {
-    hyprScripts = {
-      target = "./hypr/scripts/";
-      source = ./scripts;
-      recursive = true;
-    };
-  };
+	xdg.configFile = {
+		hyprScripts = {
+			target = "./hypr/scripts/";
+			source = ./scripts;
+			recursive = true;
+		};
+	};
 
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    "$scripts" = "~/.config/hypr/scripts";
-    monitor = [
-      "eDP-1, preferred, 1920x0, 1"
-      "HDMI-A-1, preferred, 0x0, 1"
-    ];
+	wayland.windowManager.hyprland.settings = {
+		"$mod" = "SUPER";
+		"$scripts" = "~/.config/hypr/scripts";
+		monitor = [
+			"eDP-1, preferred, 1920x0, 1"
+			"HDMI-A-1, preferred, 0x0, 1"
+		];
 
-    env = [
-      "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-      "HYPRCURSOR_THEME,${cursorName}"
-      "HYPRCURSOR_SIZE,${toString pointer.size}"
-    ];
-    xwayland.force_zero_scaling = true;
+		env = [
+			"QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+			"HYPRCURSOR_THEME,${cursorName}"
+			"HYPRCURSOR_SIZE,${toString pointer.size}"
+		];
+		xwayland.force_zero_scaling = true;
 
-    exec-once = [
-      # set cursor for HL itself
-      "nu $scripts/swk.nu 1"
-      "mako"
-      "hyprctl setcursor ${cursorName} ${toString pointer.size}"
-      "hyprlock"
-    ];
+		exec-once = [
+			# set cursor for HL itself
+			"nu $scripts/swk.nu 1"
+			"mako"
+			"hyprctl setcursor ${cursorName} ${toString pointer.size}"
+			"hyprlock"
+		];
 
-    general = {
-      border_size = 1;
-      gaps_in = 2;
-      gaps_out = 0;
-      "col.active_border" = "${cyan} ${green} 90deg";
-      "col.inactive_border" = grey;
-      layout = "master";
-    };
+		general = {
+			border_size = 1;
+			gaps_in = 2;
+			gaps_out = 0;
+			"col.active_border" = "${cyan} ${green} 90deg";
+			"col.inactive_border" = grey;
+			layout = "master";
+		};
 
-    master = {
-      mfact = 0.7;
-      new_status = "slave";
-      new_on_top = "true";
-    };
+		master = {
+			mfact = 0.7;
+			new_status = "slave";
+			new_on_top = "true";
+		};
 
-    decoration = {
-      rounding = 1;
-      active_opacity = 1.0;
-      dim_around = 0.3;
-      blur.enabled = false;
-      shadow.enabled = false;
-    };
+		decoration = {
+			rounding = 1;
+			active_opacity = 1.0;
+			dim_around = 0.3;
+			blur.enabled = false;
+			shadow.enabled = false;
+		};
 
-    input = {
-      kb_layout = "us";
-      follow_mouse = 1;
-      accel_profile = "flat";
-      touchpad = {
-        natural_scroll = "yes";
-        disable_while_typing = "yes";
-      };
-    };
+		input = {
+			kb_layout = "us";
+			follow_mouse = 1;
+			accel_profile = "flat";
+			touchpad = {
+				natural_scroll = "yes";
+				disable_while_typing = "yes";
+			};
+		};
 
-    cursor = {
-      inactive_timeout = 1;
-      hide_on_key_press = true;
-    };
+		cursor = {
+			inactive_timeout = 1;
+			hide_on_key_press = true;
+		};
 
-    gestures = {
-      # workspace_swipe = true;
-      # workspace_swipe_fingers = 3;
-      # workspace_swipe_distance = 300;
-      # workspace_swipe_forever = true;
-    };
-    misc = {
-      focus_on_activate = true;
+		gestures = {
+			# workspace_swipe = true;
+			# workspace_swipe_fingers = 3;
+			# workspace_swipe_distance = 300;
+			# workspace_swipe_forever = true;
+		};
+		misc = {
+			focus_on_activate = true;
 			on_focus_under_fullscreen = 1;
-    };
+		};
 
-    animations.enabled = true;
-  };
+		bezier = [
+			"overshot, 0.05, 0.9, 0.1, 1.05"
+			"smoothOut, 0.36, 0, 0.66, -0.56"
+			"smoothIn, 0.25, 1, 0.5, 1"
+		];
+
+		animations.enabled = true;
+
+		animation = [
+			"windows, 1, 0.5, overshot, slide"
+			"windowsOut, 1, 4, smoothOut, slide"
+			"windowsMove, 1, 4, default"
+			"border, 0"
+			"fade, 1, 10, smoothIn"
+			"fadeDim, 1, 10, smoothIn"
+			"workspaces, 1, 6, default"
+		];
+	};
 }
