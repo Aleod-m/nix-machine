95247426d47a1c71d6b0fab37cf5a8eaab0bce94
Aleod-m <adriendml99@protonmail.com>
2026-04-07 17:17:49 +0200

    Add incus preseed.

--- machines/modules/virt.nix
+++ machines/modules/virt.nix
@@ -1,5 +1,50 @@
 {...}: {
   virtualisation.containers.enable = true;
 	virtualisation.incus.enable = true;
-	networking.firewall.trustedInterfaces = [ "incusbr0" ];
+	virtualisation.incus.preseed = {
+		networks = [
+			{
+				config = {
+					"ipv4.address" = "10.0.100.1/24";
+					"ipv4.nat" = "true";
+				};
+				name = "incusbr0";
+				type = "bridge";
+			}
+		];
+		profiles = [
+			{
+				devices = {
+					eth0 = {
+						name = "eth0";
+						network = "incusbr0";
+						type = "nic";
+					};
+					root = {
+						path = "/";
+						pool = "default";
+						size = "35GiB";
+						type = "disk";
+					};
+				};
+				name = "default";
+			}
+		];
+		storage_pools = [
+			{
+				config = {
+					source = "/var/lib/incus/storage-pools/default";
+				};
+				driver = "dir";
+				name = "default";
+			}
+		];
+	};
+	networking.firewall= {
+		trustedInterfaces = [ "incusbr0" ];
+		interfaces.incusbr0 = {
+			allowedTCPPorts = [53 67];
+			allowedUDPPorts = [53 67];
+		};
+	};
 }
